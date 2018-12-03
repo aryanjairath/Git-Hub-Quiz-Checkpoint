3cb199685280f771532a6320c1f41496694f5c95
aryanjairath <aryanjairath@users.noreply.github.com>
2018-12-03 08:03:32 -0500

    Final Commit for Part1

--- Git.java
+++ Git.java
@@ -1,6 +1,10 @@
 public class Git{
 	private String word;
 		public Git(String word){
-			this.word = "This is my GitHub test";
-		}
+			word = "This is my GitHub test";
+	}
+		public String toString(){
+			return word;
+
+	}
 }
